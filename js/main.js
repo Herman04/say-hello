9577fc519ed71b735f7189c106ff4af41af50447
Herman Chenwi <herman.chenwi@gmail.com>
2016-11-29 21:35:56 -0500

    consolidating code using a variable

--- js/main.js
+++ js/main.js
@@ -1,16 +1,25 @@
-var nameInput = document.getElementById('name');
+var nameInput = document.querySelector('input');
 var form = document.querySelector('form');
 var p = document.createElement('p');
 
 nameInput.addEventListener('keypress', function (event) {
   if (event.which == 13) {
-    p.innerHTML = 'Hello ' + nameInput.value + ', nice to meet ya!';
-    console.log(p);
-    form.appendChild(p);
-    event.preventDefault();
+    if (nameInput.value.length == 0) {
+      var message = 'Oops looks like the text field is empty!'
+      writeToDocument(message);
+    } else {
+      message = 'Hello ' + nameInput.value + ', nice to meet you!';
+      writeToDocument(message);
+    }
   }
 })
 
+function writeToDocument (message) {
+  p.innerHTML = message;
+  form.appendChild(p);
+  event.preventDefault();
+
+}
 /*console log with quotes is a string*/
 
 /*nameInput.addEventListener('keypress', function (e){
