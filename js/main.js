90b42c1041e7a6d709f351cd2a4acdc2ff40b024
Herman Chenwi <herman.chenwi@gmail.com>
2016-11-29 20:38:02 -0500

    adding javascript elements to test the prompt function in the console

--- js/main.js
+++ js/main.js
@@ -1,1 +1,15 @@
 var nameInput = document.getElementById('name');
+
+nameInput.addEventListener('keypress', function (event) {
+  if (event.which == 13) {
+    console.log('Hello ' + nameInput.value + ', nice to meet ya!');
+    event.preventDefault();
+  }
+})
+
+/*console log with quotes is a string*/
+
+/*nameInput.addEventListener('keypress', function (e){
+  console.log(e)
+  e.preventDefault();
+})*/
